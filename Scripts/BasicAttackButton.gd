2c2e4d307dd073a99e513c764fc8d75441b7246e
saketdixit <saketdix@buffalo.edu>
2023-11-07 19:08:27 -0500

    first push

---
+++ Scripts/BasicAttackButton.gd
@@ -0,0 +1,17 @@
+extends "res://Scripts/ButtonComponent.gd"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_pressed():
+	print("Attack Button Pressed!")
+
+func _on_released():
+	pass
